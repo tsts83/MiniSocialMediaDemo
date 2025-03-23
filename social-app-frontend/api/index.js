7efce3ae85b7975f5a28c7de6ce780617f1085a7
tsts83 <tsts83@gmail.com>
2025-03-23 18:17:05 +0100

    fixed things

--- social-app-frontend/api/index.js
+++ social-app-frontend/api/index.js
@@ -8,10 +8,10 @@ const swaggerDocs = require('./config/swagger');
 
 const app = express();
 
-console.log('VITE_API_URL:', process.env.API_URL);
-console.log('Node environment:', process.env.NODE_ENV);
-console.log('MongoDB URI:', process.env.MONGO_URI);
-console.log('MongoDB URI:', process.env.FRONTEND_URL);
+console.error('VITE_API_URL:', process.env.API_URL);
+console.error('Node environment:', process.env.NODE_ENV);
+console.error('MongoDB URI:', process.env.MONGO_URI);
+console.error('MongoDB URI:', process.env.FRONTEND_URL);
 
 // Connect to DB
 connectDB();

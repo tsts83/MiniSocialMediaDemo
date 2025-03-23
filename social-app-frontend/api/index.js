7c37cbc88c6b977ba9b1df636a79f83234d1784a
tsts83 <tsts83@gmail.com>
2025-03-23 19:13:33 +0100

    testing

--- social-app-frontend/api/index.js
+++ social-app-frontend/api/index.js
@@ -1,23 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const swaggerDocs = require('./config/swagger');
-
 const app = express();
 
-console.error('VITE_API_URL:', process.env.API_URL);
-console.error('Node environment:', process.env.NODE_ENV);
-console.error('MongoDB URI:', process.env.MONGO_URI);
-console.error('Frontend URI:', process.env.FRONTEND_URL);
+console.log("Testing basic log output");
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
-// Swagger API Docs
-swaggerDocs(app);
-
-
-
-// Export the Express app as a serverless function
-module.exports = (req, res) => {
-    return app(req, res);
-};
+module.exports = (req, res) => app(req, res);

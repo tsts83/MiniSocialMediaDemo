65de46d8adb3e875e6e031f02d2bfa8074fc5df9
tsts83 <tsts83@gmail.com>
2025-03-23 19:22:30 +0100

    testing

--- social-app-frontend/api/index.js
+++ social-app-frontend/api/index.js
@@ -1,11 +1,66 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const swaggerDocs = require('./config/swagger');
+
 const app = express();
 
-console.log("Testing basic log output");
+console.error('VITE_API_URL:', process.env.API_URL);
+console.error('Node environment:', process.env.NODE_ENV);
+console.error('MongoDB URI:', process.env.MONGO_URI);
+console.error('Frontend URI:', process.env.FRONTEND_URL);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// Connect to DB
+connectDB();
+
+// Enable CORS
+app.use(cors({
+    origin: process.env.FRONTEND_URL,  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow sending credentials (cookies, auth headers)
+}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
 });
 
-module.exports = (req, res) => app(req, res);
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+
+// Swagger API Docs
+swaggerDocs(app);
+
+// If we're not running on Vercel, start the server locally
+if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.PORT;
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = cloudinary;
+
+// Export the Express app as a serverless function
+module.exports = (req, res) => {
+    return app(req, res);
+};

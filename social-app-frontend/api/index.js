ff60db914d458b9ebfa21a264a713a3760d54e58
tsts83 <tsts83@gmail.com>
2025-03-23 19:11:04 +0100

    testing

--- social-app-frontend/api/index.js
+++ social-app-frontend/api/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();  // Load environment variables
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
@@ -6,32 +6,18 @@ const swaggerDocs = require('./config/swagger');
 
 const app = express();
 
-// Log environment variables for debugging
 console.error('VITE_API_URL:', process.env.API_URL);
 console.error('Node environment:', process.env.NODE_ENV);
 console.error('MongoDB URI:', process.env.MONGO_URI);
 console.error('Frontend URI:', process.env.FRONTEND_URL);
 
-// Connect to MongoDB
-connectDB();
-
-// Enable CORS for all domains
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Define API routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/posts', require('./routes/postRoutes'));
 
 // Swagger API Docs
 swaggerDocs(app);
 
-// Export app for serverless
-module.exports = (req, res) => app(req, res);  // This is what Vercel expects
 
+
+// Export the Express app as a serverless function
+module.exports = (req, res) => {
+    return app(req, res);
+};
